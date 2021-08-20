49333c1ac6c4db449c15a218791d6e7ab4787140
muffin <navneetmuffin@pm.me>
2021-08-20 23:47:50 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void) {
+
+    char s[10];
+
+
+    printf("Enter a string: ");
+    scanf("%s", s);
+    printf("The string is: %s\n", s);
+
+    return 0;
+
+}
