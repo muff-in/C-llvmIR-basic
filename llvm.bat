e64e482f0f8e4e8db1dd37b4b9dbe20ae5b314dc
muffin <navneetmuffin@pm.me>
2021-08-25 12:02:44 +0530

    master

--- llvm.bat
+++ llvm.bat
@@ -1,9 +1,8 @@
 set projectName=testllvm
 
-clang %projectName%.c -S   Rem for asm file
+clang %projectName%.c -S   
+clang %projectName%.c -Ofast -S -emit-llvm -o %projectName%.ll  
 
-clang %projectName%.c -Ofast -S -emit-llvm -o %projectName%.ll  Rem for ll file
+clang %projectName%.ll -o %projectName%.exe 
 
-clang %projectName%.ll -o %projectName%.exe Rem for exe
-
-%projectName%.exe Rem ./xyz.exe
+%projectName%.exe 
